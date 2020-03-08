f27094a28dc2e7bb19b07fa2c63c08f1e3bcdc1e
Awtanabe <panser098@gmail.com>
2020-03-08 23:59:09 +0900

    redux form

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,19 +3,22 @@ import {reduxForm} from 'redux-form'
 
 class PostsNew extends Component {
   render(){
+     //handleSubmitは reduxformに入っている
+    const {fields: {title, categories, content}, handleSubmit} = this.props
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label>Title</label>
-          <input type="text" className="form-control"/>
+           {/* formProps={...title}   onChange={title.onChange} */}
+          <input type="text" className="form-control" onChange={title.onChange}/>
         </div>
         <div className="form-group">
           <label>Categories</label>
-          <input type="text" className="form-control"/>
+          <input type="text" className="form-control"  onChange={categories.onChange}/>
         </div>
         <div className="form-group">
           <label>Content</label>
-          <textarea className="form-control"/>
+          <textarea className="form-control"  onChange={content.onChange}/>
         </div>
         <button type="submit" className="btn-primary">Submit</button>
       </form>
@@ -25,7 +28,8 @@ class PostsNew extends Component {
 
 export default reduxForm ({
  form: 'PostsNewForm',
- fields: ['title', 'categories', 'content'] 
+ fields: ['title', 'categories', 'content'],
+ initialValue: {title:"", categories:"",content: "" }
 })(PostsNew)
 
 
